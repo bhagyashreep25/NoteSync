87f66af959c0acce50c6953fee7b5ef23e3a7063
NetraGhaisas <netraghaisas@gmail.com>
2019-10-05 22:16:41 +0530

    Integrated ocr

---
+++ index.php
@@ -0,0 +1,86 @@
+<html>
+
+<head>
+    <style>
+        body {
+            margin: 0;
+            font-family: Georgia, Helvetica, sans-serif;
+
+        }
+
+        .nav {
+            overflow: hidden;
+            background-image: linear-gradient(to bottom right, #23416b, #b04276);
+        }
+
+        .nav a {
+
+            float: left;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            font-size: 20px;
+            opacity: 0.8;
+        }
+
+        .nav a:hover {
+            opacity: 1;
+        }
+
+        .add {
+            margin: 5%;
+            margin-left: 28%;
+            padding: 30px;
+            border: #b04276 solid 2px;
+            width: 40%;
+        }
+
+        .add input {
+            border-radius: 7px;
+            width: 250px;
+            text-align: center;
+            background-color: bisque;
+        }
+
+        h1 {
+            color: black;
+            text-align: center;
+            font-size: 50px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1><i>NoteSync</i></h1>
+    <div class="nav">
+        <a class="Home" href="#home">Home</a>
+        <a class="Docs" href="#docs">YourDocs</a>
+        <a class="about">About</a>
+        <a class="Scan" href="#scan">Sign up</a>
+
+
+    </div>
+    <div class="add">
+        <form action="" method="post" enctype="multipart/form-data">
+            <center>
+                Add the image here:<br>
+                <br><input type="file" name="ocrfile" id="ocrfile">
+                <br><br>
+
+                <input type="submit" value="Upload" name="submit" formaction="cloud.php">
+                <br><br>
+                <input type="download" value="Download text file" name="download">
+                <br><br>
+                <input type="covert" value="Covert to pdf" name="convert">
+                <br><br>
+                <input type="text" value="Message" name="message" id="message">
+                <br><br> <input type="button" value="Audio">
+            </center>
+
+        </form>
+
+    </div>
+</body>
+
+</html>
