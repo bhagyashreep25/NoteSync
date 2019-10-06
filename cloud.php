cf80d8f5e61f862041da6e1ce2510700f6eebe97
NetraGhaisas <netraghaisas@gmail.com>
2019-10-06 08:21:48 +0530

    Changes to cloud.php

--- cloud.php
+++ cloud.php
@@ -30,7 +30,8 @@ if (isset($_FILES['image'])) {
 
     if (empty($errors) == true) {
         move_uploaded_file($file_tmp, $upload_link);
-        $query1 = "INSERT INTO $dbname.convert(userid, name, piclink) values (0, '$file_name', '$upload_link')";
+        $userid = $_SESSION['userid'];
+        $query1 = "INSERT INTO $dbname.convert(userid, name, piclink) values ($userid, '$file_name', '$upload_link')";
         $result = mysqli_query($conn, $query1);
         if ($result) {
             echo "Database entry done";
@@ -52,6 +53,7 @@ if (isset($_FILES['image'])) {
     );
 
     $image_file = "./uploads/" . $file_name;
+    print_r($image_file);
     // $image_file = 'C:/Users/Netra/Downloads/notes1.jpeg';
     // $image_file = 'D:/BHAGYASHREE/HACK/netra.jpeg'; // \SplFileObject | Image file to perform OCR on.  Common file formats such as PNG, JPEG are supported.
     $language = "ENG"; // string | Optional, language of the input document, default is English (ENG).  Possible values are ENG (English), ARA (Arabic), ZHO (Chinese - Simplified), ZHO-HANT (Chinese - Traditional), ASM (Assamese), AFR (Afrikaans), AMH (Amharic), AZE (Azerbaijani), AZE-CYRL (Azerbaijani - Cyrillic), BEL (Belarusian), BEN (Bengali), BOD (Tibetan), BOS (Bosnian), BUL (Bulgarian), CAT (Catalan; Valencian), CEB (Cebuano), CES (Czech), CHR (Cherokee), CYM (Welsh), DAN (Danish), DEU (German), DZO (Dzongkha), ELL (Greek), ENM (Archaic/Middle English), EPO (Esperanto), EST (Estonian), EUS (Basque), FAS (Persian), FIN (Finnish), FRA (French), FRK (Frankish), FRM (Middle-French), GLE (Irish), GLG (Galician), GRC (Ancient Greek), HAT (Hatian), HEB (Hebrew), HIN (Hindi), HRV (Croatian), HUN (Hungarian), IKU (Inuktitut), IND (Indonesian), ISL (Icelandic), ITA (Italian), ITA-OLD (Old - Italian), JAV (Javanese), JPN (Japanese), KAN (Kannada), KAT (Georgian), KAT-OLD (Old-Georgian), KAZ (Kazakh), KHM (Central Khmer), KIR (Kirghiz), KOR (Korean), KUR (Kurdish), LAO (Lao), LAT (Latin), LAV (Latvian), LIT (Lithuanian), MAL (Malayalam), MAR (Marathi), MKD (Macedonian), MLT (Maltese), MSA (Malay), MYA (Burmese), NEP (Nepali), NLD (Dutch), NOR (Norwegian), ORI (Oriya), PAN (Panjabi), POL (Polish), POR (Portuguese), PUS (Pushto), RON (Romanian), RUS (Russian), SAN (Sanskrit), SIN (Sinhala), SLK (Slovak), SLV (Slovenian), SPA (Spanish), SPA-OLD (Old Spanish), SQI (Albanian), SRP (Serbian), SRP-LAT (Latin Serbian), SWA (Swahili), SWE (Swedish), SYR (Syriac), TAM (Tamil), TEL (Telugu), TGK (Tajik), TGL (Tagalog), THA (Thai), TIR (Tigrinya), TUR (Turkish), UIG (Uighur), UKR (Ukrainian), URD (Urdu), UZB (Uzbek), UZB-CYR (Cyrillic Uzbek), VIE (Vietnamese), YID (Yiddish)
